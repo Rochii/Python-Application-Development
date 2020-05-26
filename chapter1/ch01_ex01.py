80eaf92dd753b7a16dedef9879219388a80c0159
Rochii <truchero.roger@gmail.com>
2020-05-26 19:16:40 +0200

    Format beautify

--- chapter1/ch01_ex01.py
+++ chapter1/ch01_ex01.py
@@ -1,9 +1,16 @@
 import random
+import textwrap
 
 def play():
 
     keep_playing = "y"
-    game_mission = (
+
+    # Print the game mission
+    width = 72
+    dotted_line = '-' * width
+    print(dotted_line)
+    print("\033[1m" + "Attack of The Orcs v0.0.1:" + "\033[0m")
+    msg = (
         "The war between humans and their arch enemies, Orcs, was in the "
         "offing. Sir Foo, one of the brave knights guarding the southern "
         "plains began a long journey towards the east through an unknown "
@@ -11,9 +18,12 @@ def play():
         " Tired and hoping to replenish his food stock, he decided to take"
         " a detour. As he approached the village, he saw five huts. There "
         "was no one to be seen around. Hesitantly, he  decided to enter..")
-
-    # Print the game mission
-    print(game_mission)
+    print(textwrap.fill(msg, width=width))
+    print("\033[1m" + "Mission:" + "\033[0m")
+    print("\tChoose a hut where Sir Foo can rest...")
+    print("\033[1m" + "TIP:" + "\033[0m")
+    print("Be careful as there are enemies lurking around!")
+    print(dotted_line)
 
     while keep_playing == "y":
         # Create huts lists and place randomly 'enemy', 'friend' or 'unoccupied' in 5 huts    
@@ -31,14 +41,16 @@ def play():
             if i+1 == num:
                 occupant_info = "\033[1m" + occupant_info + "\033[0m"
             msg += occupant_info + " "
-        print("\t" + msg + ("-"*72))
+        print("\t" + msg)
+        print(dotted_line)
         print("\033[1m" + "Entering hut %d... " % num + "\033[0m", end=' ')
         
         if huts[num-1] == "enemy":
             print("\033[1m" + "YOU LOSE! Better luck next time!" + "\033[0m")
         else:
             print("\033[1m" + "YOU WIN! Congratulations!" + "\033[0m")
-
+        
+        print(dotted_line)
         keep_playing = input("Play again? Yes(y)/No(n):")
     
 if __name__ == "__main__":
